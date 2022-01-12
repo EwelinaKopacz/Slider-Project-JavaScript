871dd62e40c22144c808b0e32c71a339a7f4743d
Ewelina <ewelina@ekopacz.pl>
2022-01-12 21:40:00 +0100

    upgrade readme text

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,7 +2,7 @@ const init = function() {
     const imagesList = document.querySelectorAll('.gallery__item');
     imagesList.forEach( img => {
         img.dataset.sliderGroupName = Math.random() > 0.5 ? 'nice' : 'good';
-    }); // za każdym przeładowaniem strony przydzielaj inną nazwę grupy dla zdjęcia
+    }); 
 
     runJSSlider();
 }
@@ -13,8 +13,8 @@ const runJSSlider = function() {
     const imagesSelector = '.gallery__item';
     const sliderRootSelector = '.js-slider';
 
-    const imagesList = document.querySelectorAll(imagesSelector); // zmienna ImagesList - nodeList ze wszystkimi zdjęciami, ktore maja klase .gallery__item
-    const sliderRootElement = document.querySelector(sliderRootSelector); //zmienna sliderRootElement - zawiera  sekcje js-slider - sekcja do pokazu slajdow
+    const imagesList = document.querySelectorAll(imagesSelector); 
+    const sliderRootElement = document.querySelector(sliderRootSelector); 
 
     initEvents(imagesList, sliderRootElement);
     initCustomEvents(imagesList, sliderRootElement, imagesSelector);
@@ -22,37 +22,26 @@ const runJSSlider = function() {
 
 const initEvents = function(imagesList, sliderRootElement) {
     imagesList.forEach( function(item)  {
-        item.addEventListener('click', function(e) { // do kazdego zdjecia dodajemy nasluchiwanie na klikniecie i wywolujemy f-cje fireCustomEvent
-            fireCustomEvent(e.currentTarget, 'js-slider-img-click'); // wywołanie funkcji, ktora ma dwa argumenty, e.currentTarget - element do ktorego podpielismy f-cje nasluchujaca dane zdarzenie, i string 'js-slider-img-click'
+        item.addEventListener('click', function(e) {
+            fireCustomEvent(e.currentTarget, 'js-slider-img-click'); 
         });
     });
 
-    // todo:
-    // utwórz nasłuchiwanie eventu o nazwie [click], który ma uruchomić event [js-slider-img-next]
-    // na elemencie [.js-slider__nav--next]
     const navNext = sliderRootElement.querySelector('.js-slider__nav--next');
     navNext.addEventListener('click', function(e){
-        fireCustomEvent(e.currentTarget, 'js-slider-img-next'); // e.currentTarget - element do ktorego podpielismy f-cje nasluchujaca dane zdarzenie
+        fireCustomEvent(e.currentTarget, 'js-slider-img-next');
         e.stopPropagation();
     });
 
-    // todo:
-    // utwórz nasłuchiwanie eventu o nazwie [click], który ma uruchomić event [js-slider-img-prev]
-    // na elemencie [.js-slider__nav--prev]
     const navPrev = sliderRootElement.querySelector('.js-slider__nav--prev');
     navPrev.addEventListener('click',function(e){
         fireCustomEvent(e.currentTarget, 'js-slider-img-prev');
         e.stopPropagation();
     });
 
-
-    // todo:
-    // utwórz nasłuchiwanie eventu o nazwie [click], który ma uruchomić event [js-slider-close]
-    // tylko wtedy, gdy użytkownik kliknie w [.js-slider__zoom]
     const zoom = sliderRootElement.querySelector('.js-slider__zoom');
-    zoom.addEventListener('click',function(e){ // zdarzenie 'click' i eventHandler - function(e)
-        // if(e.target === zoom){ // alternatywa do e.stopPropagation()
-        fireCustomEvent(e.currentTarget,'js-slider-close');// e.currentTarget - cel zdarzenia, bedacy obiektem kontekstu
+    zoom.addEventListener('click',function(e){ 
+        fireCustomEvent(e.currentTarget,'js-slider-close');
         // }
     })
 }
@@ -60,33 +49,26 @@ const initEvents = function(imagesList, sliderRootElement) {
 const fireCustomEvent = function(element, name) {
     console.log(element.className, '=>', name);
 
-    const event = new CustomEvent(name, { // nazwa customEvent to "name"? nie jest w ' '?
-        bubbles: true,                    // drugi parametr czyli dodatkowe ustawienia, parametr opcjonalny
+    const event = new CustomEvent(name, {
+        bubbles: true,
     });
 
-    element.dispatchEvent( event ); // na kazdym elemencie wykonuje (wywołanie) customEvent = 'event'??
+    element.dispatchEvent( event );
 }
 
 const initCustomEvents = function(imagesList, sliderRootElement, imagesSelector) {
     imagesList.forEach(function(img) {
-        img.addEventListener('js-slider-img-click', function(event) { //nasluchiwanie na wszystkie zdjcia, przypięte własne zdarzenie "js-slider-img-click"
+        img.addEventListener('js-slider-img-click', function(event) {
             onImageClick(event, sliderRootElement, imagesSelector);
         });
     });
 
-    sliderRootElement.addEventListener('js-slider-img-next', onImageNext); // wywołanie własnych zdarzen
+    sliderRootElement.addEventListener('js-slider-img-next', onImageNext); 
     sliderRootElement.addEventListener('js-slider-img-prev', onImagePrev);
-    sliderRootElement.addEventListener('js-slider-close', onClose); //sliderRootSelector = '.js-slider';
+    sliderRootElement.addEventListener('js-slider-close', onClose); 
 }
 
-const onImageClick = function(event, sliderRootElement, imagesSelector) {// (event, section-js-slider, klasa-.gallery__item)
-    // todo:
-    // 1. dodać klasę [.js-slider--active], aby pokazać całą sekcję
-    // 2. wyszukać ściężkę (atrybut [src]) do klikniętego elementu i wstawić do [.js-slider__image]
-    // 3. pobrać nazwę grupy zapisaną w dataset klikniętego elementu
-    // 4. wyszukać wszystkie zdjęcia należące do danej grupy, które wykorzystasz do osadzenia w dolnym pasku
-    // 5. utworzyć na podstawie elementu [.js-slider__thumbs-item--prototype] zawartość dla [.js-slider__thumbs]
-    // 6. zaznaczyć przy pomocy klasy [.js-slider__thumbs-image--current], który element jest aktualnie wyświetlany
+const onImageClick = function(event, sliderRootElement, imagesSelector) {
 
     sliderRootElement.classList.add('js-slider--active');
 
@@ -112,7 +94,7 @@ const onImageClick = function(event, sliderRootElement, imagesSelector) {// (eve
             }
         });
 
-    if(sliderThumb && thumbPrototype){ //lepsze rozwiązanie metoda forEach()
+    if(sliderThumb && thumbPrototype){
         for(i=0; i<thumbGroup.length; i++){
 
         const cloneThumb = thumbPrototype.cloneNode(true);
@@ -135,13 +117,6 @@ const onImageClick = function(event, sliderRootElement, imagesSelector) {// (eve
 
 const onImageNext = function(event) {
     console.log(this, 'onImageNext');
-    // [this] wskazuje na element [.js-slider]
-    // todo:
-    // 1. wyszukać aktualny wyświetlany element przy pomocy [.js-slider__thumbs-image--current]
-    // 2. znaleźć element następny do wyświetlenie względem drzewa DOM dla [.js-slider__thumbs]
-    // 3. sprawdzić czy ten element istnieje - jeśli nie to [.nextElementSibling] zwróci [null]
-    // 4. przełączyć klasę [.js-slider__thumbs-image--current] do odpowiedniego elementu
-    // 5. podmienić atrybut o nazwie [src] dla [.js-slider__image]
 
     const currentImagClassName = 'js-slider__thumbs-image--current';
     const emptyImgElement = 'js-slider__thumbs-item--prototype';
@@ -173,13 +148,6 @@ const onImageNext = function(event) {
 
 const onImagePrev = function(event) {
     console.log(this, 'onImagePrev');
-    // [this] wskazuje na element [.js-slider]
-    // todo:
-    // 1. wyszukać aktualny wyświetlany element przy pomocy [.js-slider__thumbs-image--current]
-    // 2. znaleźć element poprzedni do wyświetlenie względem drzewa DOM dla [.js-slider__thumbs]
-    // 3. sprawdzić czy ten element istnieje i czy nie posiada klasy [.js-slider__thumbs-item--prototype]
-    // 4. przełączyć klasę [.js-slider__thumbs-image--current] do odpowiedniego elementu
-    // 5. podmienić atrybut [src] dla [.js-slider__image]
 
     const currentImagClassName = 'js-slider__thumbs-image--current';
     const emptyImgElement = 'js-slider__thumbs-item--prototype';
@@ -198,7 +166,7 @@ const onImagePrev = function(event) {
         const curretSliderImg = document.querySelector('.js-slider__image');
         curretSliderImg.setAttribute('src',imgSrc);
     }
-    else { // ZADANIE DODATKOWE NR 1
+    else {
         lastImg.querySelector('img').classList.add(currentImagClassName)
         parentCurrentImg.querySelector('img').classList.remove(currentImagClassName);
 
@@ -209,12 +177,9 @@ const onImagePrev = function(event) {
 }
 
 const onClose = function(event) {
-    // todo:
-    // 1. należy usunać klasę [js-slider--active] dla [.js-slider]
-    // 2. należy usunać wszystkie dzieci dla [.js-slider__thumbs] pomijając [.js-slider__thumbs-item--prototype]
 
-    console.log(event.target); //js-slider__zoom
-    console.log(event.currentTarget) //js-slider
+    console.log(event.target);
+    console.log(event.currentTarget)
 
     const zoom = event.currentTarget.querySelector('.js-slider__zoom');
         if(event.target === zoom){
